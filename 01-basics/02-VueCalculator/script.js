1d7be6ec4a57ee1f1d0b75f97d55793a568cabe8
Olga Denisyuk <denisykolga@yandex.ru>
2023-11-28 20:23:20 +0300

    Solve 01-basics(1-3 tasks)

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,44 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { defineComponent } from 'vue';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+
+    name: 'App',
+
+    data() {
+        return {
+            value1: null,
+            value2: null,
+            result: null,
+            operation: ''
+        };
+    },
+
+    methods: {
+        sum() {
+            this.result = this.value1 + this.value2;
+            this.operation = 'operator1'
+        },
+        subtract() {
+            this.result = this.value1 - this.value2;
+            this.operation = 'operator2'
+        },
+        multiply() {
+            this.result = this.value1 * this.value2;
+            this.operation = 'operator3'
+        },
+        divide() {
+            this.result = this.value1 / this.value2;
+            this.operation = 'operator4'
+        },
+        changeResult() {
+            this.operation === 'operator1' ? this.sum : '';
+            this.operation === 'operator2' ? this.subtract : '';
+            this.operation === 'operator3' ? this.multiply : '';
+            this.operation === 'operator4' ? this.divide : '';
+        },
+    },
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
