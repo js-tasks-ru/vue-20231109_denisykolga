6eb8a67ac6468471ed8029b2ee3d4cfa8e1b216c
Olga Denisyuk <denisykolga@yandex.ru>
2023-12-11 22:59:19 +0300

    Solve MeetupView

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,38 +1,74 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
+import MeetupDescription from './MeetupDescription.js';
+import MeetupCover from './MeetupCover.js';
+import MeetupInfo from './MeetupInfo.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgenda from './MeetupAgenda.js';
+import meetups from '../api/meetups.js';
 
 export default defineComponent({
-  name: 'MeetupView',
+    name: 'MeetupView',
 
-  components: {
-    UiAlert,
-    UiContainer,
-  },
-
-  props: {
-    meetup: {
-      type: Object,
-      required: true,
+    components: {
+        UiAlert,
+        UiContainer,
+        MeetupDescription,
+        MeetupCover,
+        MeetupInfo,
+        MeetupAgendaItem,
+        MeetupAgenda
     },
-  },
 
-  template: `
+    data() {
+        return {
+            meetups,
+            selectedMeetup: 0,
+        };
+    },
+
+    props: {
+        meetup: {
+            type: Object,
+            required: true,
+        },
+    },
+    computed: {
+        description() {
+            return this.meetups[this.selectedMeetup].description;
+        },
+    },
+
+    template: `
     <div>
       <!-- meetup cover-->
-
+      <MeetupCover :title="meetups[selectedMeetup].title" :image="meetups[selectedMeetup].image" />
       <UiContainer>
         <div class="meetup">
           <div class="meetup__content">
+            <p>
+            <select v-model="selectedMeetup">
+            <option v-for="(meetup, index) in meetups" :key="meetup.id" :value="index">
+              {{ index }}: {{ meetup.title }}
+            </option>
+            </select>
+          </p>
             <h3>Описание</h3>
             <!-- meetup description -->
-
+            <MeetupDescription :description="description" />
             <h3>Программа</h3>
             <!-- meetup agenda -->
-            <UiAlert>Программа пока пуста...</UiAlert>
+            <MeetupAgenda :agenda="meetups[selectedMeetup].agenda" />
+            <UiAlert v-if="meetups[selectedMeetup].agenda.length===0">Программа пока пуста...</UiAlert>
           </div>
           <div class="meetup__aside">
             <!-- meetup info -->
+            <MeetupInfo
+        :organizer="meetups[selectedMeetup].organizer"
+        :place="meetups[selectedMeetup].place"
+        :date="meetups[selectedMeetup].date"
+      />
           </div>
         </div>
       </UiContainer>
