0db03a6d5afa36b35dac0a658011eaba222938ac
Olga Denisyuk <denisykolga@yandex.ru>
2023-12-11 11:21:08 +0300

    Solve MeetupAgenda

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,13 +1,22 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
-  name: 'MeetupAgenda',
+    name: 'MeetupAgenda',
 
-  template: `
+    props: {
+        agenda: Array,
+        //required: true,
+    },
+
+    components: {
+        MeetupAgendaItem,
+    },
+
+    template: `
     <ul class="agenda">
-      <li class="agenda__item">
-        <!-- meetup agenda item -->
+      <li class="agenda__item" v-for="item in agenda">
+      <MeetupAgendaItem :agenda-item="item" />
       </li>
     </ul>`,
 });
