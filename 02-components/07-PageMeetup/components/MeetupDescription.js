a35e1a9c945330fc3bf1e87e97d73914066777ca
Olga Denisyuk <denisykolga@yandex.ru>
2023-12-20 23:18:01 +0300

    PageMeetup

--- 02-components/07-PageMeetup/components/MeetupDescription.js
+++ 02-components/07-PageMeetup/components/MeetupDescription.js
@@ -2,11 +2,9 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 
 export default defineComponent({
     name: 'MeetupDescription',
-
     props: {
         description: {
             type: String,
-            required: true,
         },
     },
 
