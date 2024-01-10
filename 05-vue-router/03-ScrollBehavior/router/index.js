49a85cf596b31122d1fb15875066c24a16e4b6cb
Olga Denisyuk <denisykolga@yandex.ru>
2024-01-10 18:02:58 +0300

    Solve ScrollBehavior

---
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -0,0 +1,59 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+export const router = createRouter({
+    history: createWebHistory('/05-vue-router/03-ScrollBehavior'),
+
+    routes: [{
+            path: '/',
+            name: 'index',
+            component: () =>
+                import ('../views/PageMeetups.vue'),
+        },
+        {
+            path: '/meetups',
+            name: 'meetups',
+            redirect: { name: 'index' },
+        },
+        {
+            path: '/meetups/:meetupId(\\d+)',
+            name: 'meetup',
+            meta: {
+                showReturnToMeetups: true,
+                saveScrollPosition: true,
+            },
+            props: true,
+            redirect: (to) => ({ name: 'meetup.description', params: to.params }),
+            component: () =>
+                import ('../views/PageMeetup.vue'),
+            children: [{
+                    path: '',
+                    alias: 'description',
+                    name: 'meetup.description',
+                    props: true,
+                    component: () =>
+                        import ('../views/PageMeetupDescription.vue'),
+                },
+                {
+                    path: 'agenda',
+                    name: 'meetup.agenda',
+                    props: true,
+                    component: () =>
+                        import ('../views/PageMeetupAgenda.vue'),
+                },
+            ],
+        },
+    ],
+
+    scrollBehavior(to, from, savedPosition) {
+        if (to.hash) {
+            return {
+                el: to.hash,
+            }
+        }
+
+        if (to.meta.saveScrollPosition && from.meta.saveScrollPosition) {
+            return false;
+        }
+        return savedPosition || { top: 0, left: 0 }
+    }
+});
